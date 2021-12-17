dde4ec38381168304ec33ad76755fb9b5de4d39d
yaruo-react-redux <kazuya@goemon-the-web.com>
2021-12-16 16:20:56 -0800

    fix index.js message

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['webpack', '動いてるお〜'], ' ');
 
   return element;
 }
