e4000d78599c519d1f0f2275899580f504a399f5
yaruo-react-redux <kazuya@goemon-the-web.com>
2022-01-20 00:43:46 -0800

    DiaryCardHeader refactoring done

--- src/components/DiaryCard.tsx
+++ src/components/DiaryCard.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 import { Diary } from '../diaryData';
+import DiaryCardHeader from './DiaryHeader';
 
 export type DiaryCardProps = {
   diary: Diary;
@@ -62,20 +59,7 @@ const DiaryCard = (props: DiaryCardProps) => {
 
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label='recipe'>
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label='settings'>
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title={title}
-        subheader={`postID:${diaryId}-${postDate}`}
-      />
+      <DiaryCardHeader diaryId={diaryId} title={title} postDate={postDate} />
       <CardMedia
         component='img'
         height='194'
